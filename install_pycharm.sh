19a54fe80c2956cf67f3992906b34b36eef7632b
Andrei Kosarev <kosarev_andrey@mail.ru>
2024-12-28 14:18:31 +0100

    added pycharm

---
+++ install_pycharm.sh
@@ -0,0 +1,12 @@
+wget https://download.jetbrains.com/python/pycharm-community-2024.3.1.1.tar.gz
+sudo tar -C /opt -zxf pycharm-community-2024.3.1.1.tar.gz
+
+echo 'export PATH="$PATH:/opt/pycharm-community-2024.3.1.1/bin"' >> ~/.bashrc
+
+sudo rm pycharm-community-2024.3.1.1.tar.gz
+
+
+
+
+
+
